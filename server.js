9c8b5f2aacc42c829c5f9581c9ddbdd9c7599c46
Mr-puffer <59946562+Mr-puffer@users.noreply.github.com>
2020-05-25 07:57:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var http=require('http')
-var server=http.createServer((function(request,response)
-{
-	fs.readFile("index.html", function(err, data){
-   if(err){
-      response.writeHead(404);
-      response.write("Not Found!");
-   }
-   else{
-      response.writeHead(200, {'Content-Type': contentType});
-      response.write(data);
-   }
-   response.end();
-});
-
+var http = require('http');
+var fs = require('fs');
+//2.
+var server = http.createServer(function (req, resp) {
+    //3.
+    if (req.url === "/") {
+        fs.readFile("index.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
 server.listen(7000);
