d1c9dded94816d0324e6c56a72d00d0231cf944f
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2020-12-11 07:12:26 -0500

    add LICENSE

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ var addlbotperms = {};
 var mutelist = [];
 
 
-var version = '1.5.3b';
+var version = '1.5.3b.1';
 global.updateStatus = async () => {
   let newStatus = props.feat.status ? props.feat.status.replace('{prefix}', defaultprefix).replace('{guilds}', client.guilds.cache.size) : null;
   let currentStatus;
