7b95b75b247e417fdc8995182ef8a27a000fa858
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2021-01-30 15:45:48 -0500

    remove ghostdot, ghostdot_delmsg

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ var addlbotperms = {};
 var mutelist = [];
 
 
-var version = '1.5.7b';
+var version = '1.5.7c';
 global.updateStatus = async () => {
   let newStatus = props.feat.status ? props.feat.status.replace('{prefix}', defaultprefix).replace('{guilds}', client.guilds.cache.size) : null;
   let currentStatus;
