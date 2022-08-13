e01b4a25d4259d5cfdad0225c2aac2278a5d9ca6
coolguy284 <coolguy284v64@gmail.com>
2022-08-13 04:13:15 -0400

    update rel path to fix docker scripts
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- docker/canary/start.sh
+++ docker/canary/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker run --rm -it --name thebotcat-canary \
-  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../.env")",target=/home/thebotcat/.env,readonly \
-  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../props.json")",target=/home/thebotcat/props.json \
+  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../.env")",target=/home/thebotcat/.env,readonly \
+  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../props.json")",target=/home/thebotcat/props.json \
   -e VERSION='canary' \
   --memory 0.8g --memory-swap 0.8g --memory-swappiness 0 thebotcat/thebotcat
--- docker/normal/start.sh
+++ docker/normal/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker run --rm -it --name thebotcat \
-  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../.env")",target=/home/thebotcat/.env,readonly \
-  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../props.json")",target=/home/thebotcat/props.json \
+  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../.env")",target=/home/thebotcat/.env,readonly \
+  --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../props.json")",target=/home/thebotcat/props.json \
   -e VERSION='normal' \
   --memory 1.5g --memory-swap 1.5g --memory-swappiness 0 thebotcat/thebotcat
