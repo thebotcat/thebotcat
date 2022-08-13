5cbd49ddb3e300fd130935255bfe65baf132ad15
coolguy284 <coolguy284v64@gmail.com>
2022-08-13 01:10:23 -0400

    add memory arguments to start and create fix perms
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ docker/fix_perms.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "${0%/*}/.."
+
+sudo chmod 700 .
+sudo chmod -R 770 ./* ./.[^.]*
+sudo chown 2201 ./.env
+sudo chown 2201 ./props.json
--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,4 @@
 sudo docker run --rm -it --name thebotcat \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../.env")",target=/home/thebotcat/.env,readonly \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../props.json")",target=/home/thebotcat/props.json \
-  thebotcat/thebotcat
+  --memory 1.5g --memory-swap 1.5g --memory-swappiness 0 thebotcat/thebotcat
