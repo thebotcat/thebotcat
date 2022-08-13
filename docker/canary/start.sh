bbfc6d2a6af818aa556b606ba0f866efcd31b409
coolguy284 <coolguy284v64@gmail.com>
2022-08-13 04:06:08 -0400

    fix typo in docker scripts
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- docker/canary/start.sh
+++ docker/canary/start.sh
@@ -3,5 +3,5 @@
 sudo docker run --rm -it --name thebotcat-canary \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../.env")",target=/home/thebotcat/.env,readonly \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../props.json")",target=/home/thebotcat/props.json \
-  -e VERSION='canary'
+  -e VERSION='canary' \
   --memory 0.8g --memory-swap 0.8g --memory-swappiness 0 thebotcat/thebotcat
--- docker/normal/start.sh
+++ docker/normal/start.sh
@@ -3,5 +3,5 @@
 sudo docker run --rm -it --name thebotcat \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../.env")",target=/home/thebotcat/.env,readonly \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../props.json")",target=/home/thebotcat/props.json \
-  -e VERSION='normal'
+  -e VERSION='normal' \
   --memory 1.5g --memory-swap 1.5g --memory-swappiness 0 thebotcat/thebotcat
