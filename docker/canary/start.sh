fde86b41f1378566f7c274a3ffd412ed3a88b00b
coolguy284 <coolguy284v64@gmail.com>
2023-07-28 02:16:33 -0400

    update botcat memory usage defaults
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- docker/canary/start.sh
+++ docker/canary/start.sh
@@ -4,4 +4,4 @@ sudo docker run --rm -it --name thebotcat-canary \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../.env")",target=/home/thebotcat/.env,readonly \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../props.json")",target=/home/thebotcat/props.json \
   -e VERSION='canary' \
-  --memory 0.8g --memory-swap 0.8g --memory-swappiness 0 thebotcat/thebotcat
+  --memory 0.7g --memory-swap 2.0g thebotcat/thebotcat
--- docker/normal/start.sh
+++ docker/normal/start.sh
@@ -4,4 +4,4 @@ sudo docker run --rm -it --name thebotcat \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../.env")",target=/home/thebotcat/.env,readonly \
   --mount type=bind,source="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../props.json")",target=/home/thebotcat/props.json \
   -e VERSION='normal' \
-  --memory 1.5g --memory-swap 1.5g --memory-swappiness 0 thebotcat/thebotcat
+  --memory 0.7g --memory-swap 2.0g thebotcat/thebotcat
