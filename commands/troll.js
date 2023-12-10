b36df8de7ad50af7a84647399b23e9dbaca1bead
coolguy284 <coolguy284v64@gmail.com>
2023-12-10 06:36:31 -0500

    add slash command support for ghostdot
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- commands/troll.js
+++ commands/troll.js
@@ -59,7 +59,27 @@ module.exports = [
   {
     name: 'ghostdot',
     description: '`!ghostdot #channel1 [#channel2 ...]` to send a message and delete it in a certain channel or channels\n`!ghostdot all` to send a message then immediately delete it in every channel in a guild',
-    flags: 0b010100,
+    description_slash: 'sends a message then deletes it in a certain channel or channels',
+    flags: 0b110100,
+    options: [
+      {
+        type: Discord.ApplicationCommandOptionType.Subcommand,
+        name: 'all',
+        description: 'ghostdots every channel',
+        options: [
+          { type: Discord.ApplicationCommandOptionType.Boolean, name: 'ephemeral', description: 'whether the command and result are visible to only you, defaults to false' }
+        ],
+      },
+      {
+        type: Discord.ApplicationCommandOptionType.Subcommand,
+        name: 'one',
+        description: 'ghostdots one channel',
+        options: [
+          { type: Discord.ApplicationCommandOptionType.Channel, name: 'channel', description: 'the channel', required: true },
+          { type: Discord.ApplicationCommandOptionType.Boolean, name: 'ephemeral', description: 'whether the command and result are visible to only you, defaults to false' },
+        ],
+      },
+    ],
     async execute(o, msg, rawArgs) {
       if (!persData.special_guilds_set.has(msg.guild.id)) return;
       if (!(common.isDeveloper(msg) || common.isOwner(msg) || common.isAdmin(msg)))
@@ -87,6 +107,39 @@ module.exports = [
               await new Promise(r => setTimeout(r), 400);
             }
           }
+        }
+      }
+    },
+    async execute_slash(o, interaction, command, args) {
+      if (!persData.special_guilds_set.has(o.guild.id)) return;
+      if (!(common.isDeveloper(o) || common.isOwner(o) || common.isAdmin(o)))
+        return;
+      
+      switch (args[0].name) {
+        case 'all': {
+          if (persData.special_guilds_set.has(o.guild.id)) nonlogmsg(`ghostdot from ${o.author.tag} in ${o.guild ? o.guild.name + ':' + o.channel.name : 'dms'}: all`);
+          await common.slashCmdResp(o, args[0].options[0] ? args[0].options[0].value : false, 'ghostdot');
+          let channels = Array.from(o.guild.channels.cache.values()), message;
+          for (let i = 0; i < channels.length; i++) {
+            if (channels[i] && (channels[i].type == Discord.ChannelType.GuildText || channels[i].type == Discord.ChannelType.GuildAnnouncement)) {
+              message = await channels[i].send('ghostdot');
+              try { await message.delete(); } catch (e) { }
+              await new Promise(r => setTimeout(r), 400);
+            }
+          }
+          break;
+        }
+        
+        case 'one': {
+          if (persData.special_guilds_set.has(o.guild.id)) nonlogmsg(`ghostdot from ${o.author.tag} in ${o.guild ? o.guild.name + ':' + o.channel.name : 'dms'}: ${args[0].options[0].value}`);
+          await common.slashCmdResp(o, args[0].options[1] ? args[0].options[1].value : false, 'ghostdot');
+          channel = o.guild.channels.cache.get(args[0].options[0].value);
+          if (channel && (channel.type == Discord.ChannelType.GuildText || channel.type == Discord.ChannelType.GuildAnnouncement)) {
+            message = await channel.send('ghostdot');
+            try { await message.delete(); } catch (e) { }
+            await new Promise(r => setTimeout(r), 400);
+          }
+          break;
         }
       }
     }
@@ -123,6 +176,6 @@ module.exports = [
           }
         }
       }
-    }
+    },
   },
 ];
